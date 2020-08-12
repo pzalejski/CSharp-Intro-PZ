c2b7808e55d5abad2e7be6c46145bd7cdb2e19e4
pzalejski <piotrzalejski@outlook.com>
2020-08-11 19:50:35 -0500

    introduction to if statements and conditional statements - pz

--- Decisions/Decisions/Program.cs
+++ Decisions/Decisions/Program.cs
@@ -6,31 +6,38 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Peter's Big Giveaway!");
             Console.Write("Choose a door: 1, 2, or 3: ");
             string userValue = Console.ReadLine();
 
             string message = "";
 
-            if (userValue =="1")
-            {
+            if (userValue == "1")
                 message = "You won a new car!";
-            }
-            else if (userValue =="2")
-            {
+            else if (userValue == "2")
                 message = "You won free lunch!";
-            }
+
             else if (userValue == "3")
-            {
                 message = "You won a new boat!";
-            }
             else
             {
                 message = "Sorry, I don't understand.";
+                message += "You Lose. ";
             }
+
             Console.WriteLine(message);
             Console.ReadLine();
+            */
 
+
+            Console.WriteLine("Peter's Big Giveaway!");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You won a {0}." , message);
+            Console.ReadLine();
         }
     }
 }
