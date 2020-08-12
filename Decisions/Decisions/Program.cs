7fa495f50104667b96e1f4490777317dc25c0552
pzalejski <piotrzalejski@live.com>
2020-08-11 19:19:18 -0500

    first commit-pz

---
+++ Decisions/Decisions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peter's Big Giveaway!");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue =="1")
+            {
+                message = "You won a new car!";
+            }
+            else if (userValue =="2")
+            {
+                message = "You won free lunch!";
+            }
+            else if (userValue == "3")
+            {
+                message = "You won a new boat!";
+            }
+            else
+            {
+                message = "Sorry, I don't understand.";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
+// A method is a block of code that has a name in the above case the name Main
+// Main method lives inside a class called Program
+// Namespace overly-simplified is just another way of organizing code
