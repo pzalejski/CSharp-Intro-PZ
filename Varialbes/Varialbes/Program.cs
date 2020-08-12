0c45c0a5eaac2e26a10393b8e8ff28caf1064416
pzalejski <piotrzalejski@outlook.com>
2020-08-11 20:02:51 -0500

    introduction to variables in C# - pz

---
+++ Varialbes/Varialbes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Varialbes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastname;
+            Console.Write("Type your last name: ");
+            myLastname = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0} {1}", myFirstName, myLastname);
+        }
+    }
+}
