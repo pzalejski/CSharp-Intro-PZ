33020f4f5da2447810e0fd5aa255f85f329474cd
pzalejski <piotrzalejski@outlook.com>
2020-08-26 19:47:22 -0500

    introduction to methods -pz

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+            //Create a helper method, create and call methods to retrieve a value, create and use input parameters, learn about string formatting, and create overloaded methods.//
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
